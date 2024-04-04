a346b06cfa4e9d8b0df7a910bc1802cd5590d2cf
Ricardo Wagner <32727717+erredablio@users.noreply.github.com>
2024-04-03 23:47:19 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+def decisor():
+    decisor = int(input("\nDeseja realizar uma nova operação? [1 - Sim] [2 - Não]: "))
+    match decisor:
+        case 1:
+            calculadora()
+        case _:
+            exit
+
+def somar(n1, n2):
+    equacao = n1 + n2
+    print(f"\n{n1} + {n2} = {equacao}")
+
+def subtrair(n1, n2):
+    equacao = n1 - n2
+    print(f"\n{n1} - {n2} = {equacao}")
+
+def multiplicacao(n1, n2):
+    equacao = n1 * n2
+    print(f"\n{n1} * {n2} = {equacao}")
+
+def divisao(n1, n2):
+    equacao = n1 / n2
+    print(f"\n{n1} / {n2} = {equacao:.2f}")
+
+def calculadora():
+    n1 = float(input("Informe o primeiro valor: "))
+    n2 = float(input("Informe o segundo valor: "))
+    operador = input("Selecione o operador [+] [-] [*] [/]: ")
+    match operador:
+        case "+":
+            somar(n1, n2)
+            decisor()
+        case "-":
+            subtrair(n1, n2)
+            decisor()
+        case "*":
+            multiplicacao(n1, n2)
+            decisor()
+        case "/":
+            divisao(n1, n2)
+            decisor()
+        case _:
+            print("Operador inválido")
+            decisor()
+
+if __name__ == "__main__":
+    calculadora()
